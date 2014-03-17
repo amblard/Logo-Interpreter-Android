79eed2bd9b44c8152f45b6c8dd818d05ca76c3f3
Vincent Guillebaud <vincent.guillebaud@gmail.com>
2014-03-17 19:03:24 +0100

    Reconnaissance et gestion des commandes tactiles (gestures)

--- Android/src/com/example/parser/MainActivity.java
+++ Android/src/com/example/parser/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.parser;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.GestureOverlayView.OnGesturePerformedListener;
+import android.gesture.Prediction;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.Display;
@@ -10,14 +18,15 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity implements OnClickListener, OnGesturePerformedListener {
 
 	private Button boutonParser;
 
 	private EditText userinput;
 	private Editable input;
-
+	private GestureLibrary gestureLibrary;
 	public static int largeurEcran, hauteurEcran;
 
 	@Override
@@ -36,7 +45,15 @@ public class MainActivity extends Activity implements OnClickListener {
 		userinput.setMaxHeight((int) (hauteurEcran/2.9));
 		userinput.setWidth(largeurEcran);
 		userinput.setMaxWidth(largeurEcran);
-		
+
+
+		gestureLibrary = GestureLibraries.fromRawResource(this, R.raw.gestures);
+		   if (!gestureLibrary.load()) {
+		     finish();
+		   }
+		 
+		   GestureOverlayView gestures = (GestureOverlayView) findViewById(R.id.gestures);
+		   gestures.addOnGesturePerformedListener(this);
 		
 
 	}
@@ -52,6 +69,52 @@ public class MainActivity extends Activity implements OnClickListener {
 		input = userinput.getText();
 		runDrawCanvas();
 
+	}
+
+	@Override
+	public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+		
+		// on récupère les prédictions faites par la reconaissance de mouvement
+		ArrayList<Prediction> predictions = gestureLibrary.recognize(gesture);
+		 
+		   if (predictions.size() > 0 && predictions.get(0).score > 1.0) {
+		     String result = predictions.get(0).name;
+		     int cursorPosition = userinput.getSelectionStart(); // on récupère la position du curser
+		     String input = userinput.getText().toString();
+		     String debut = input.subSequence(0, cursorPosition).toString();
+		     String fin= input.subSequence(cursorPosition, input.length()).toString();
+		     String cmdToAdd;
+		     
+		     // c'est un cercle -> repeat
+		     if ("circle".equalsIgnoreCase(result)) {
+		    	 cmdToAdd = "\nREPEAT [ ]";
+		    	 userinput.setText(debut+ cmdToAdd + fin);
+		    	 userinput.setSelection(debut.length()+cmdToAdd.length());
+		       //Toast.makeText(this, "CIRCLE", Toast.LENGTH_LONG).show();
+		     } 
+		     // dessin d'un LT
+		     else if ("lt".equalsIgnoreCase(result)) {
+		    	 cmdToAdd = "\nLT ";
+		    	 userinput.setText(debut+ cmdToAdd + fin);
+		    	 userinput.setSelection(debut.length()+cmdToAdd.length());
+		      // Toast.makeText(this, "LT", Toast.LENGTH_LONG).show();
+		     }
+		     // dessin d'un RT
+		     else if ("rt".equalsIgnoreCase(result)) {
+		    	 cmdToAdd = "\nRT ";
+		    	 userinput.setText(debut+ cmdToAdd + fin);
+		    	 userinput.setSelection(debut.length()+cmdToAdd.length());
+			       //Toast.makeText(this, "RT", Toast.LENGTH_LONG).show();
+			 }
+		     // ATTENTION le fd ne peut pas être reconnu car un "gesture" doit être composé d'au moins "deux traits"
+		     else if ("fd".equalsIgnoreCase(result)) {
+			       Toast.makeText(this, "FD", Toast.LENGTH_LONG).show();
+			 }
+		     else {
+		    	 Toast.makeText(this, "Commande non reconnue", Toast.LENGTH_LONG).show();
+		     }
+		   }
+		
 	}
 
 }
